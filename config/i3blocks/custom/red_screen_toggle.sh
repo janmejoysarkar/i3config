4781fb0dda01fa5b8e4c8984eb723152befa8e1a
Janmejoy Sarkar <63224620+janmejoysarkar@users.noreply.github.com>
2024-01-18 23:52:42 +0530

    Add files via upload

--- config/i3blocks/custom/red_screen_toggle.sh
+++ config/i3blocks/custom/red_screen_toggle.sh
@@ -2,11 +2,9 @@
 state=$(xrandr --verbose | grep -i gamma | head -n1 | cut -d':' -f4)
 if [ $state == "1.0" ]
 then
-	xrandr --output eDP-1 --gamma 1:0.7:0.5
-	xrandr --output HDMI-1 --gamma 1:0.7:0.5 --brightness 0.7
+	xrandr --output `xrandr --listactivemonitors | tail -1 | awk '{print $4}'` --gamma 1:0.6:0.5
 	echo "❨"
 else
-	xrandr --output eDP-1 --gamma 1:1:1
-        xrandr --output HDMI-1 --gamma 1:1:1 --brightness 1
+	xrandr --output `xrandr --listactivemonitors | tail -1 | awk '{print $4}'` --gamma 1:1:1
 	echo "⦿"
 fi
